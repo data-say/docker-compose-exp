83cc5a29943d085f609790aaf54031fbc9a96cef
lee.sy <lee.sy@gsretail.com>
2024-05-07 21:14:57 +0900

    feat: add env

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import time
-
+from os import environ as env
 import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host=env["HOST"], port=env["PORT"])
 
 def get_hit_count():
     retries = 5
